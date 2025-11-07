7c2c44cb850c0378b7b5255eadf3949a4e46c610
johnreybaluca46-lab <johnreybaluca46@gmail.com>
2025-11-08 02:20:12 +0800

    Add files via upload

---
+++ home-functional.js
@@ -0,0 +1,103 @@
+// front end developer show
+var displays = 1;
+
+function fed() {
+
+    // front end developer page
+    var fed1 = document.getElementById('fed-open');
+    
+    if (displays == 1) {
+        fed1.style.display = "block";
+        displays = 1;
+    }
+}
+
+var displays2 = 1;
+function fedclose() {
+
+    // front end developer close page
+    var fed2 = document.getElementById('fed-open');
+    
+    if (displays == 1) {
+        fed2.style.display = "none";
+        displays2 = 1;
+    }
+}
+
+// html and css show
+var displays3 = 1;
+
+function hac() {
+
+    // html and css page
+    var fed3 = document.getElementById('hac-open');
+    
+    if (displays == 1) {
+        fed3.style.display = "block";
+        displays3 = 1;
+    }
+}
+
+var displays4 = 1;
+function hacclose() {
+
+    // html and css close page
+    var fed4 = document.getElementById('hac-open');
+    
+    if (displays == 1) {
+        fed4.style.display = "none";
+        displays4 = 1;
+    }
+}
+
+// danicing
+var displays5 = 1;
+
+function dcg() {
+
+    // dancing page
+    var fed5 = document.getElementById('dcg-open');
+    
+    if (displays == 1) {
+        fed5.style.display = "block";
+        displays5 = 1;
+    }
+}
+
+var displays6 = 1;
+function dcgclose() {
+
+    // danicng close page
+    var fed6 = document.getElementById('dcg-open');
+    
+    if (displays == 1) {
+        fed6.style.display = "none";
+        displays6 = 1;
+    }
+}
+
+// cooking
+var displays7 = 1;
+
+function ckg() {
+
+    // cooking page
+    var fed7 = document.getElementById('ckg-open');
+    
+    if (displays == 1) {
+        fed7.style.display = "block";
+        displays7 = 1;
+    }
+}
+
+var displays8 = 1;
+function ckgclose() {
+
+    // cooking close page
+    var fed8 = document.getElementById('ckg-open');
+    
+    if (displays == 1) {
+        fed8.style.display = "none";
+        displays8 = 1;
+    }
+}
